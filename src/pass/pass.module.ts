71f0a82d79c12cb1ee873894c943636a86ac51f3
verhuurplatvorm <informatiehetstrand@gmail.com>
2026-08-21 23:46:21 +0200

    Fix: PassModule registreerde WalletPassService/GoogleWalletService niet als provider

--- src/pass/pass.module.ts
+++ src/pass/pass.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassController } from './pass.controller';
+import { WalletPassService } from '../wallet/wallet-pass.service';
+import { GoogleWalletService } from '../common/google-wallet.service';
 
 @Module({
   controllers: [PassController],
+  providers: [WalletPassService, GoogleWalletService],
 })
 export class PassModule {}
